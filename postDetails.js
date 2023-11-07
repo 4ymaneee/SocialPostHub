4a46c94ffa66f907bb9ea51a4c086147a7243770
4ymaneee <nalmlwmyat4@gmail.com>
2023-11-07 21:35:41 +0100

    update

--- postDetails.js
+++ postDetails.js
@@ -1,6 +1,130 @@
 //BASE URL
 const baseURL = "https://tarmeezacademy.com/api/v1";
 
+// Get the URL parameter
+let urlParams = new URLSearchParams(window.location.search);
+let postId = urlParams.get("postId");
+console.log(postId); // This will log the 'id' from the URL
+
+//Get Post Details
+function getPostDetails() {
+  axios
+    .get(`https://tarmeezacademy.com/api/v1/posts/${postId}`)
+    .then((response) => {
+      let post = response.data.data;
+      let postContent = document.getElementById("posts");
+      // console.log(post);
+      let token = localStorage.getItem("token");
+      postContent.innerHTML = "";
+      let owner = post.author.username;
+      let username = post.author.username;
+      let profilePic = post.author.profile_image;
+      let postImage = post.image;
+      let timeCreated = post.created_at;
+      let postTitle = post.title;
+      let body = post.body;
+      let commentsCount = post.comments_count;
+
+      let emptyTitle = "";
+      if (postTitle == null) {
+        postTitle = emptyTitle;
+      }
+      let tags = post.tags;
+      let tag = "";
+      for (let i = 0; i < tags.length; i++) {
+        tag += `<span class="btn btn-secondary rounded-pill me-1">${tags[i].name}</span>`;
+      }
+      //Get Comments
+      let usernameOfComment = post.comments;
+
+      let commentContent = post.comments;
+      let profilePicOfComment = post.comments;
+      let comments = post.comments;
+      let commentHTML = "";
+      for (let z = 0; z < comments.length; z++) {
+        commentHTML += `<div class="comment" style="display: flex; padding-left: 12px; margin-top: 14px; border-bottom: 1px solid rgba(0, 0, 0, 0.226);">
+                        <img src="${profilePicOfComment[z].author.profile_image}" alt="" style="width: 47px; height: 47px;"
+                            class="rounded-circle border border-3 profile-pic">
+                        <div class="comment-content" style="margin-left: 7px;">
+                            <b>@${usernameOfComment[z].author.username}</b>
+                          <p>${commentContent[z].body} </p>
+                        </div>
+                    </div>`;
+      }
+      //Add Comments
+      let AddComment = "";
+      if (token != null) {
+        AddComment = `<div class="add-comment" style="width: 100%; height: 59px; border: 1px solid rgba(0, 0, 0, 0.226) ; padding: 4px 0 4px 0;">
+                              <input type="text" style="width: 83%; height: 100%; border: none; padding-left: 10px; outline: none;" placeholder="Write a Comment..." class="comment-input">
+                              <button class="btn btn-primary send-btn" style="width: 16%; height: 100%; ;" onclick="createNewComment()">Send</button>
+                      </div>`;
+      } else {
+        AddComment = "";
+      }
+      postContent.innerHTML = `<h1 class="mt-5 owner" style="font-weight: 700;" >@${owner} Post</h1>
+      <div class="card shadow my-5">
+              <div class="card-header" >
+                  <img src="${profilePic}" alt="" style="width: 40px; height: 40px;"
+                      class="rounded-circle border border-3 profile-pic">
+                  <b >@${username}</b>
+              </div>
+              <div class="card-body" onclick="changeLocation()" style="cursor: pointer;">
+                  <img class="w-100 rounded"
+                      src="${postImage}"
+                      alt="">
+                  <h6 class="mt-2" style="color: grey;">${timeCreated}</h6>
+                  <h5>${postTitle}</h5>
+                  <p>${body}</p>
+                  <hr>
+                  <div>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                          class="bi bi-pen" viewBox="0 0 16 16">
+                          <path
+                              d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
+                      </svg>
+                      <span class="me-2">(${commentsCount}) comments</span>
+  
+                      <i id="tag-content"> 
+                      ${tag}
+                      </i>
+                  </div>
+              </div>
+              <div class="comments" style="display: flex; flex-direction: column; background-color: rgba(164, 225, 255, 0.603);">
+                      ${commentHTML}      
+              </div>
+              ${AddComment}
+      </div>`;
+    });
+}
+
+getPostDetails(postId);
+
+//Create Comment
+function createNewComment() {
+  let comment = document.querySelector(".comment-input").value;
+  let token = localStorage.getItem("token");
+  let data = {
+    body: comment,
+  };
+
+  let config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  axios
+    .post(`${baseURL}/posts/${postId}/comments`, data, config)
+    .then(function (response) {
+      console.log(response);
+      showSuccessAlert("Comment Added Successfully");
+      getPostDetails(postId);
+    })
+    .catch(function (error) {
+      console.log(error);
+      showDangerAlert("You Have to Login First");
+    });
+}
+
 //Login User
 function loginBtnClicked() {
   let username = document.getElementById("username-input").value;
@@ -19,6 +143,7 @@ function loginBtnClicked() {
       modalInstance.hide();
       showSuccessAlert("You Have Login successfully");
       setupUI();
+      getPostDetails();
     })
     .catch(function (error) {
       showDangerAlert(error.response.data.message);
@@ -105,7 +230,9 @@ function logoutUser() {
   document.querySelector(".logout").style.display = "none";
   document.querySelector(".profile-pic").style.display = "none";
   username.style.display = "none";
-  document.querySelector(".new").style.display = "none";
+  // document.querySelector(".new").style.display = "none";
+  console.log("hy");
+  getPostDetails();
 }
 
 // Register User
@@ -135,6 +262,7 @@ function registerBtnClicked() {
       localStorage.setItem("user", JSON.stringify(response.data.user));
       showSuccessAlert("New User Registered Successfully");
       setupUI();
+      getPostDetails();
     })
     .catch(function (error) {
       showDangerAlert(error.response.data.message);
@@ -173,122 +301,5 @@ function createBtnClicked() {
     .catch(function (error) {
       let message = error.response.data.message;
       showDangerAlert(message);
-    });
-}
-
-// Get the URL parameter
-let urlParams = new URLSearchParams(window.location.search);
-let postId = urlParams.get("postId");
-console.log(postId); // This will log the 'id' from the URL
-
-//Get Post Details
-function getPostDetails(id) {
-  axios
-    .get(`https://tarmeezacademy.com/api/v1/posts/${id}`)
-    .then((response) => {
-      let post = response.data.data;
-      let postContent = document.getElementById("posts");
-      // console.log(post);
-
-      postContent.innerHTML = "";
-      let owner = post.author.username;
-      let username = post.author.username;
-      let profilePic = post.author.profile_image;
-      let postImage = post.image;
-      let timeCreated = post.created_at;
-      let postTitle = post.title;
-      let body = post.body;
-      let commentsCount = post.comments_count;
-
-      let emptyTitle = "";
-      if (postTitle == null) {
-        postTitle = emptyTitle;
-      }
-      let tags = post.tags;
-      let tag = "";
-      for (let i = 0; i < tags.length; i++) {
-        tag += `<span class="btn btn-secondary rounded-pill me-1">${tags[i].name}</span>`;
-      }
-      //Get Comments
-      let usernameOfComment = post.comments;
-
-      let commentContent = post.comments;
-      let profilePicOfComment = post.comments;
-      let comments = post.comments;
-      let commentHTML = "";
-      for (let z = 0; z < comments.length; z++) {
-        commentHTML += `<div class="comment" style="display: flex; padding-left: 12px; margin-top: 14px; border-bottom: 1px solid rgba(0, 0, 0, 0.226);">
-                        <img src="${profilePicOfComment[z].author.profile_image}" alt="" style="width: 47px; height: 47px;"
-                            class="rounded-circle border border-3 profile-pic">
-                        <div class="comment-content" style="margin-left: 7px;">
-                            <b>@${usernameOfComment[z].author.username}</b>
-                          <p>${commentContent[z].body} </p>
-                        </div>
-                    </div>`;
-      }
-      postContent.innerHTML = `<h1 class="mt-5 owner" style="font-weight: 700;" >@${owner} Post</h1>
-      <div class="card shadow my-5">
-              <div class="card-header" >
-                  <img src="${profilePic}" alt="" style="width: 40px; height: 40px;"
-                      class="rounded-circle border border-3 profile-pic">
-                  <b >@${username}</b>
-              </div>
-              <div class="card-body" onclick="changeLocation()" style="cursor: pointer;">
-                  <img class="w-100 rounded"
-                      src="${postImage}"
-                      alt="">
-                  <h6 class="mt-2" style="color: grey;">${timeCreated}</h6>
-                  <h5>${postTitle}</h5>
-                  <p>${body}</p>
-                  <hr>
-                  <div>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                          class="bi bi-pen" viewBox="0 0 16 16">
-                          <path
-                              d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
-                      </svg>
-                      <span class="me-2">(${commentsCount}) comments</span>
-  
-                      <i id="tag-content"> 
-                      ${tag}
-                      </i>
-                  </div>
-              </div>
-              <div class="comments" style="display: flex; flex-direction: column; background-color: rgba(164, 225, 255, 0.603);">
-                      ${commentHTML}      
-              </div>
-              <div class="add-comment" style="width: 100%; height: 59px; border: 1px solid rgba(0, 0, 0, 0.226) ; padding: 4px 0 4px 0;">
-                        <input type="text" style="width: 83%; height: 100%; border: none; padding-left: 10px; outline: none;" placeholder="Write a Comment..." class="comment-input">
-                        <button class="btn btn-primary send-btn" style="width: 16%; height: 100%; ;" onclick="createNewComment()">Send</button>
-              </div>
-      </div>`;
-    });
-}
-
-getPostDetails(postId);
-
-//Create Comment
-function createNewComment(id) {
-  let comment = document.querySelector(".comment-input").value;
-  let token = localStorage.getItem("token");
-  let data = {
-    body: comment,
-  };
-
-  let config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  axios
-    .post(`${baseURL}/posts/${postId}/comments`, data, config)
-    .then(function (response) {
-      console.log(response);
-      showSuccessAlert("Comment Added Successfully");
-      getPostDetails(postId);
-    })
-    .catch(function (error) {
-      console.log(error);
-      showDangerAlert("You Have to Login First");
     });
 }
