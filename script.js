ca956f6fa016149be3ce490a798b6ef4a3126f7a
4ymaneee <nalmlwmyat4@gmail.com>
2023-11-07 22:38:40 +0100

    update

--- script.js
+++ script.js
@@ -46,11 +46,17 @@ function getPosts(reload = true, page = 1) {
         tag += `<span class="btn btn-secondary rounded-pill me-1">${tags[i].name}</span>`;
       }
       allPosts.innerHTML += `<div class="card shadow my-5">
-            <div class="card-header" >
-                <img src="${profilePic}" alt="" style="width: 40px; height: 40px;"
-                    class="rounded-circle border border-3 profile-pic">
-                <b >@${username}</b>
-            </div>
+      <div class="card-header d-flex justify-content-between">
+      <div>
+          <img src="${profilePic} " alt="" style="width: 40px; height: 40px;"
+              class="rounded-circle border border-3 profile-pic">
+          <b>@${username} </b>
+      </div>
+      <button class="btn btn-primary" onclick="editPost()" data-bs-toggle="modal" data-bs-target="#edit-post-modal"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+    </svg></button>
+     </div>
             <div class="card-body" onclick="changeLocation(${postId})" style="cursor: pointer;">
                 <img class="w-100 rounded"
                     src="${postImage}"
@@ -259,4 +265,9 @@ function createBtnClicked() {
 //Change Page Location to Post Details
 function changeLocation(postId) {
   window.location = `post-details.html?postId=${postId}`;
+}
+
+//Edit Post
+function editPost() {
+  console.log("hy");
 }
